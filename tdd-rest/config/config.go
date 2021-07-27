8d7dd47fced9a55224fb1d4ab84ae3969c8d11e5
Riska Kurnia Dewi <riskakurniad@gmail.com>
2021-07-27 12:04:55 +0700

    fixing typo database name

--- tdd-rest/config/config.go
+++ tdd-rest/config/config.go
@@ -1,6 +1,6 @@
 package config
 
-const DB_CONNECTION_STRING = "root:welcome12345@tcp(localhost:3306)/rest_schemak?charset=utf8&parseTime=True&loc=Local"
+const DB_CONNECTION_STRING = "root:welcome12345@tcp(localhost:3306)/rest_schema?charset=utf8&parseTime=True&loc=Local"
 const TEST_DB_CONNECTION_STRING = "root:welcome12345@tcp(localhost:3306)/test_schema?charset=utf8&parseTime=True&loc=Local"
 
 /*
